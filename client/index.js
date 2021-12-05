35a8d54a161fa11a3912986c60aff680cc937ae6
Whoin Lee <whoinlee@gmail.com>
2021-12-05 07:17:12 -0500

    cleaned up

--- client/index.js
+++ client/index.js
@@ -3,44 +3,42 @@ import "style.css";
 import "favicon.ico";
 import io from "socket.io-client";
 import { csv } from "d3-request";
-// import { utcDay, utcMinute } from "d3-time";
 import { utcFormat } from "d3-time-format";
 
 //-- dimensions
-const width = 750;
+const width = 800;
 const height = 600;
-const margin = { top: 30, right: 100, bottom: 30, left: 20 },
+const margin = { top: 30, right: 80, bottom: 30, left: 20 },
   innerWidth = width - margin.left - margin.right,
   innerHeight = height - margin.top - margin.bottom;
+
 //-- HTML elements
 const contentDiv = document.getElementById("content");
 let chartDiv1, chartDiv2;
 let chartHolder1, chartHolder2;
 let indicationHolder1, indicationHolder2;
 let tickerSelection;
+
 //-- time related
 const formatTime = utcFormat("%H:%M");
+
 //-- chart related
-// const curve = d3.curveLinear;
 const tickers = ["AAPL", "GOOGL", "FB", "MSFT"];
 const colors = ["#1f77b4", "#9467bd", "#ff7f02", "#8c564b"];
 const upColor = "#2ca02c";
 const downColor = "#d62728";
 
 //-- load historical data
-const col = (d) => {
-  d.utcTime = formatTime(d.timestamp);
-  d.price = +d.price;
-  return d;
-};
-csv("/market-history", col, (error, data) => {
+csv("/market-history", (error, data) => {
   if (error) {
     contentDiv.textContent = error.target.response;
     return;
   }
-  //-- {timestamp: '1638351000000', ticker: 'AAPL', price: '119.25', utcTime: '09:30'}
+  contentDiv.textContent = "";
   const dataByTicker = d3.group(data, (d) => d.ticker);
-  buildCharts(dataByTicker);
+  // console.log("index.js :: buildChart1, dataByTicker?? ", dataByTicker);
+  priceChart(dataByTicker);
+  changeChart(dataByTicker);
 });
 
 //-- subscribe to updates
@@ -52,15 +50,7 @@ socket.on("start new day", function (data) {
   console.log("NewDay", data);
 });
 
-function buildCharts(data) {
-  contentDiv.textContent = ""; //-- clear
-  buildChart1(data);
-  buildChart2(data);
-}
-
-function buildChart1(dataByTicker) {
-  console.log("index.js :: buildChart1, dataByTicker?? ", dataByTicker);
-
+function priceChart(dataByTicker) {
   chartDiv1 = contentDiv.appendChild(document.createElement("div"));
   chartDiv1.setAttribute("id", "chartDiv1");
   chartDiv1.setAttribute("class", "chartDiv");
@@ -82,10 +72,12 @@ function buildChart1(dataByTicker) {
   `;
   chartHolder1 = document.getElementById("chartHolder1");
   indicationHolder1 = document.getElementById("indicationHolder1");
+
   let tickerInfo = chartDiv1.querySelector(".ticker-info");
   let tickerBlock = chartDiv1.querySelector(".ticker-info .block");
   let changeInfo = chartDiv1.querySelector(".change-info");
 
+  //-- ticker select drop-down
   tickerSelection = chartDiv1.appendChild(document.createElement("select"));
   tickerSelection.setAttribute("id", "tickerSelection");
   d3.select("#tickerSelection")
@@ -96,28 +88,36 @@ function buildChart1(dataByTicker) {
     .text((d) => d)
     .attr("value", (d) => d);
 
-  const svg = d3
-    .select("#chartHolder1")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height + margin.top + margin.bottom) /* TODO */
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+  //-- on tickerSelection change, update chart
+  d3.select("#tickerSelection").on("change", (e) =>
+    update(e.target.selectedIndex)
+  );
 
-  let selectedIndex = 0;
-  let selectedTicker = tickers[selectedIndex];
-  let selectedColor = colors[selectedIndex];
-  let selectedData = dataByTicker.get(tickers[selectedIndex]);
+  let selectedIndex, selectedTicker, selectedColor, selectedData;
 
-  //-- info color
-  tickerInfo.style.color = selectedColor;
-  tickerBlock.style.backgroundColor = selectedColor;
+  function update(index, isChartBuilt = true) {
+    selectedIndex = index;
+    selectedTicker = tickers[index];
+    selectedColor = colors[index];
+    selectedData = dataByTicker.get(tickers[index]);
+    updateInfo();
+    if (isChartBuilt) updateChart();
+  }
+
+  update(0, false);
 
   //-----------------------------//
   //-------- init chart ---------//
   //-----------------------------//
+  const svg = d3
+    .select("#chartHolder1")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g");
+
   const xValue = (d) => d["timestamp"];
-  const yValue = (d) => d["price"];
+  const yValue = (d) => +d["price"];
 
   //-- set ranges
   let xScale = d3
@@ -129,26 +129,25 @@ function buildChart1(dataByTicker) {
     .domain(d3.extent(selectedData, yValue))
     .range([innerHeight, 0]);
 
-  //-- set grids
+  //-- set grids :: vertical xGrid and horizontal yGrid
   let xGrid = (g) =>
     g
       .attr("class", "vline")
       .selectAll("line")
       .data(xScale.ticks(10))
-      // .data(xScale.ticks())
       .join("line")
       .attr("x1", (d) => xScale(d))
       .attr("x2", (d) => xScale(d))
-      .attr("y1", margin.top - 8) /*margin.top*/
-      .attr("y2", height - margin.bottom + 8); /*margin.bottom*/
+      .attr("y1", 0 + margin.top - 8) /* 8px extra long */
+      .attr("y2", height - margin.bottom + 8); /* 8px extra long */
   let yGrid = (g) =>
     g
       .attr("class", "hline")
       .selectAll("line")
       .data(yScale.ticks(5))
       .join("line")
-      .attr("x1", 0) /* margin.left(yaxis loc) - margin.left) */
-      .attr("x2", width - margin.right + 54)
+      .attr("x1", 0)
+      .attr("x2", innerWidth + 75) /* 75px extra wide */
       .attr("y1", (d) => yScale(d))
       .attr("y2", (d) => yScale(d));
   let xGridG = svg
@@ -167,17 +166,16 @@ function buildChart1(dataByTicker) {
     .attr("class", "xAxis")
     .attr(
       "transform",
-      `translate(${margin.left}, ${height - margin.top + 10})`
+      `translate(${margin.left}, ${height - margin.bottom + 8})`
     );
   xAxisB
     .call(d3.axisBottom(xScale).ticks(10).tickFormat(formatTime))
     .call((g) => g.select(".domain").remove());
-
   let xAxisT = svg
     .append("g")
     .attr("id", "xAxisT")
     .attr("class", "xAxis")
-    .attr("transform", `translate(${margin.left}, ${margin.top - 10})`);
+    .attr("transform", `translate(${margin.left}, ${margin.top - 8})`);
   xAxisT
     .call(d3.axisTop(xScale).ticks(10).tickFormat(formatTime))
     .call((g) => g.select(".domain").remove());
@@ -212,19 +210,35 @@ function buildChart1(dataByTicker) {
     .style("stroke-width", 2)
     .style("fill", "none");
 
-  updateInfo(selectedIndex);
+  function updateInfo() {
+    //-- ticker info color change
+    tickerInfo.style.color = selectedColor;
+    tickerBlock.style.backgroundColor = selectedColor;
 
-  function update(index) {
-    selectedIndex = index;
-    selectedData = dataByTicker.get(tickers[index]);
-    selectedTicker = tickers[index];
-    selectedColor = colors[index];
-
-    updateChart(index);
-    updateInfo(index);
+    //-- calculate priceChange and percentChange
+    var firstPrice = selectedData[0].price;
+    var lastPrice = selectedData[selectedData.length - 1].price;
+    var priceChange = Math.round((lastPrice - firstPrice) * 100) / 100;
+    var percentChange = Math.round((priceChange / firstPrice) * 10000) / 100;
+    var sign = priceChange == 0 ? "" : "+";
+    changeInfo.style.color = upColor;
+    if (priceChange < 0) {
+      sign = "-";
+      changeInfo.style.color = downColor;
+    }
+    lastPrice = Math.round(lastPrice * 100) / 100;
+    //
+    chartDiv1.querySelector(".ticker").textContent = selectedTicker;
+    indicationHolder1.querySelector(".price").textContent = "$" + lastPrice;
+    indicationHolder1.querySelector(".percent").innerHTML = `${sign}${Math.abs(
+      percentChange
+    )}<span>%</span>`;
+    indicationHolder1.querySelector(".value").textContent =
+      sign + "$" + Math.abs(priceChange);
   }
 
-  function updateChart(index) {
+  function updateChart() {
+    //-- update scales
     xScale = d3
       .scaleUtc()
       .domain(d3.extent(selectedData, xValue))
@@ -234,20 +248,22 @@ function buildChart1(dataByTicker) {
       .domain(d3.extent(selectedData, yValue))
       .range([innerHeight, 0]);
 
+    //-- update grids
     xGridG.call(xGrid);
     yGridG.call(yGrid);
 
+    //-- update ticks
     xAxisB
       .call(d3.axisBottom(xScale).ticks(10).tickFormat(formatTime))
       .call((g) => g.select(".domain").remove());
     xAxisT
       .call(d3.axisTop(xScale).ticks(10).tickFormat(formatTime))
       .call((g) => g.select(".domain").remove());
-
     yAxis
       .call(d3.axisRight(yScale).ticks(5).tickFormat(d3.format(".2f")))
       .call((g) => g.select(".domain").remove());
 
+    //-- update graph line
     line
       .datum(selectedData)
       .transition()
@@ -261,37 +277,8 @@ function buildChart1(dataByTicker) {
           .y((d) => yScale(d.price))
       );
   }
-
-  function updateInfo(index) {
-    tickerInfo.style.color = selectedColor;
-    tickerBlock.style.backgroundColor = selectedColor;
-
-    var firstPrice = selectedData[0].price;
-    var lastPrice = selectedData[selectedData.length - 1].price;
-    var priceChange = Math.round((lastPrice - firstPrice) * 100) / 100;
-    var percentChange = Math.round((priceChange / firstPrice) * 10000) / 100;
-    var sign = priceChange == 0 ? "" : "+";
-    changeInfo.style.color = upColor;
-    if (priceChange < 0) {
-      sign = "-";
-      changeInfo.style.color = downColor;
-    }
-    lastPrice = Math.round(lastPrice * 100) / 100;
-    chartDiv1.querySelector(".ticker").textContent = selectedTicker;
-    indicationHolder1.querySelector(".price").textContent = "$" + lastPrice;
-    indicationHolder1.querySelector(".percent").innerHTML = `${sign}${Math.abs(
-      percentChange
-    )}<span>%</span>`;
-    indicationHolder1.querySelector(".value").textContent =
-      sign + "$" + Math.abs(priceChange);
-  }
-
-  //-- when the button is changed, run the update function
-  d3.select("#tickerSelection").on("change", (e) =>
-    update(e.target.selectedIndex)
-  );
 }
 
-function buildChart2(dataByTicker) {
+function changeChart(dataByTicker) {
   // console.log("index.js :: buildChart2, dataByTicker? " + dataByTicker);
 }
